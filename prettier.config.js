7f4d26e8a863f0bd9f3431fb622c60624015225a
Piotr Siwek <31476986+piotr-siwek@users.noreply.github.com>
2023-09-04 22:34:52 +0200

    config

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	semi: true, // średnik
+	singleQuote: false,
+	trailingComma: "all",
+	printWidth: 100,
+	useTabs: true,
+	plugins: ["prettier-plugin-tailwindcss"],
+	tailwindConfig: "./tailwind.config.ts",
+};
